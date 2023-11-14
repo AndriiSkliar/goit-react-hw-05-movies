7635f859b01b60bfbd2bee4ac5acb0575fd02b66
Андрей Скляр <andreas.skliar@gmail.com>
2023-11-14 13:30:05 +0200

    base commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import { HomePage } from "pages/HomePage";
+import { MovieDetails } from "pages/MovieDetails";
+import { Movies } from "pages/Movies";
+import { Routes, Route, NavLink } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </main>
     </div>
   );
 };
